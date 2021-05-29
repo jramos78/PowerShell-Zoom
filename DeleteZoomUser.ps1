802d94a68e0f574dde4cdc75946a057f85eedc85
jramos78 <67175767+jramos78@users.noreply.github.com>
2021-05-29 08:33:50 -0400

    Update DeleteZoomUser.ps1

--- DeleteZoomUser.ps1
+++ DeleteZoomUser.ps1
@@ -7,7 +7,7 @@ function DeleteZoomUser {
 	#Get the user's Zoom Id
 	$userId = ((Invoke-RestMethod -Uri 'https://api.zoom.us/v2/users?page_size=500&status=active' -Method GET -Headers $headers).users | Where email -eq $Email).id
 	#Set the URI for the API call
-  $uri = "https://api.zoom.us/v2/users/" + $userId + "?action=delete" 
+  	$uri = "https://api.zoom.us/v2/users/" + $userId + "?action=delete" 
 	#Delete the user's account 
 	Invoke-RestMethod -Uri $uri -Method DELETE -Headers $headers
 } 
